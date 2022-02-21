76037a4676c9671b394409436354678495358339
Fernando Pereyra <fpereyra@inxap.com>
2022-02-21 09:43:22 -0300

    agrega conexion a bd

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path:'variables.env'});
+//conectamos a la bd
+mongoose.connect(process.env.DATABASE,{useNewUrlParser:true});
+//Linea que nos permite sabe si hay al error en la conexion
+mongoose.connection.on('error',error =>{
+    console.log(error)
+});
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const router = require('./router');
 const exphbs = require('express-handlebars');
 const path = require('path');
+require('dotenv').config({path: 'variables.env'}); // path nos permite endicar el directorio y llamoas a las variables meidante process.env.VARIABLE
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
@@ -16,9 +23,19 @@ app.set('view engine','handlebars');
 
 //Archivos esticos - precisa requerir path - utilidad incluida en nodejs
 app.use(express.static(path.join(__dirname,'public')));
+//CookieParser
+app.use(cookieParser());
+//Express-session
+app.use(session({
+    secret:process.env.SECRETO,
+    key:process.env.KEY,
+    resave:false,
+    saveUninitialized:false,
+    store: MongoStore.create({mongoUrl: process.env.DATABASE})
+}));
 
 //Ruteo
 app.use('/',router());
 
 //Escucha en puerto
-app.listen(5000);
+app.listen(process.env.PUERTO);
