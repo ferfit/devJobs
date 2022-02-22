7ae58ccde4eaac1d7f6ac38c4eba4a7352280375
Fernando Pereyra <fpereyra@inxap.com>
2022-02-22 13:23:49 -0300

    agrega listado de vacantes dinamico

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,9 +1,20 @@
+const mongoose = require("mongoose");
+const Vacante = mongoose.model('Vacante');
 
-exports.mostrarTrabajos = (req,res) => {
+
+exports.mostrarTrabajos = async (req,res,next) => {
+    
+    //traemos todas las vacante
+    const vacantes = await Vacante.find().lean();
+
+    if(!vacantes) return next();
+    
+    
     res.render('home',{
         nombrePagina: 'devJobs',
         tagline:'Encuentra y publica trabajos para desarrolladores web',
         barra:true,
-        boton:true
+        boton:true,
+        vacantes
     })
 }
